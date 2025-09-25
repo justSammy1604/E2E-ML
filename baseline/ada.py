e8e8573a29f89b200eb1f3ba2f2742f28cd52dc2
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-09-25 05:50:44 +0530

    Fix import statement for polars in ada.py

--- baseline/ada.py
+++ baseline/ada.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, classification_report
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from src.feat_scale import X_train_scaled, X_test_scaled, y_train, y_test, X
-import polars as pl
+import polars as pl 
 pl.Config.set_tbl_rows(100)
 
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
