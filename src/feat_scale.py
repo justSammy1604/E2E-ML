a00493cc291f771b8763cf7c8ca224987aae2b3b
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-09-26 10:08:21 +0530

    Update feat_scale.py

--- src/feat_scale.py
+++ src/feat_scale.py
@@ -5,7 +5,7 @@ import pandas as pd
 import polars as pl 
 
 data = pl.read_csv("data/diabetes_complete_clean.csv")
-X = data.drop("diabetes_binary")
+X = data.drop("diabetes_binary") 
 y = data["diabetes_binary"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
